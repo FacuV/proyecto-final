4c0055f8afadbc5e6a0ad600f49fd34e1c23a36c
FacuV <facuun.vicente@gmail.com>
2022-11-17 19:56:06 -0300

    Add login form

--- item-1/app.py
+++ item-1/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import re
 from db.config import get_db_connection
 from utils.functions import create_user
 
@@ -6,30 +7,43 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    cur, conn = get_db_connection()
-    users = cur.execute('SELECT * FROM Users')
     return render_template('signin.html')
 
 @app.route("/new/user", methods = ['POST'])
 def newUser():
     cur, conn  = get_db_connection()
-    with conn:
-        username = request.json['username']
-        fullname = request.json['fullname']
-        password = request.json['password']
-        isadmin = request.json['isadmin']
-        
-        user = username, fullname, password, isadmin
+    try:
+        with conn:
+            username = request.form['username']
+            fullname = request.form['fullname']
+            password = request.form['password']
 
-        create_user(conn, cur, user)
 
-        return render_template('signin.html' )
+            if len(username) < 4:
+                 raise Exception("El nombre de usuario debe tener una longitud mayor a 4 caracteres")
+            if not re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+                 raise Exception("La contraseña no cumple los requisitos")
+            
+            if username and fullname and password:
+                user = username, fullname, password, 0
 
-@app.route("/users", methods = ['GET'])
-def allUsers():
-    cur, conn = get_db_connection()
+                create_user(conn, cur, user)
+
+                return render_template('users.html')
+            else:
+                raise Exception("Debes ingresar todos los campos")
+    except Exception as error:
+        myError = error
+        print(myError)
+        return {
+            'error': True,
+            'message': f'Algo salió mal'
+        }
+
+@app.route("/users")
+def getUsers():
+    cur, conn  = get_db_connection()
     users = cur.execute('SELECT * FROM Users')
-    
     return render_template('users.html', users=users)
 
 if __name__ == '__main__':
