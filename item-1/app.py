320c88013c6be60f7704cbcd44edcebfb237a856
FacuV <facuun.vicente@gmail.com>
2022-11-18 20:03:04 -0300

    Casi login

--- item-1/app.py
+++ item-1/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import re
 from db.config import get_db_connection
-from utils.functions import create_user
+from utils.functions import create_user, logIn
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def main():
     return render_template('signin.html')
 
-@app.route("/new/user", methods = ['POST'])
+@app.route("/signup", methods = ['POST'])
 def newUser():
     cur, conn  = get_db_connection()
     try:
@@ -32,19 +32,43 @@ def newUser():
                 return render_template('users.html')
             else:
                 raise Exception("Debes ingresar todos los campos")
-    except Exception as error:
-        myError = error
-        print(myError)
+    except Exception:
         return {
             'error': True,
             'message': f'Algo salió mal'
         }
+
+@app.route("/login", methods = ['POST'])
+def login():
+    cur, conn  = get_db_connection()
+    # try:
+    with conn:
+        username = request.form['username']
+        password = request.form['password']
+
+        response = logIn(conn, cur, username, password)
+        return {
+            'error': False,
+            'message': response
+        }
+
+    # except Exception:
+    #     return {
+    #         'error': True,
+    #         'message': f'Algo salió mal'
+    #     }
 
 @app.route("/users")
 def getUsers():
     cur, conn  = get_db_connection()
     users = cur.execute('SELECT * FROM Users')
     return render_template('users.html', users=users)
+
+
+
+@app.route("/search")
+def searchImages():
+    return render_template('finder.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- item-1/utils/functions.py
+++ item-1/utils/functions.py
@@ -14,3 +14,21 @@ def create_user(conn, cur, user):
     conn.commit()
 
     return cur.lastrowid
+
+def logIn(conn, cur, username, password):
+    """
+    Inserts the user into the database
+    
+    :param conn: the connection to the database
+    :param cur: the cursor object
+    :param user: a tuple of the form (username, fullname, password, isAdmin)
+    :return: The user id
+    """
+    sql = f'''SELECT Username, Password FROM Users WHERE Username=username AND Password=password'''
+    print('Username: ', username)
+    print('Password: ', password)
+    cur.execute(sql, {'username': username, 'password': password})
+    
+    response = cur.fetchall() 
+
+    return response
